039987c5a063435b305bc764b80bf8c92e7ff1de
Thierno <sa.thierno@gmail.com>
2021-12-28 09:36:34 +0100

    add new files

---
+++ exemple.js
@@ -0,0 +1,58 @@
+let prenom = "lucie";
+console.log(prenom);
+let age = 21;
+console.log(age);
+let majorite = true;
+console.log(majorite)
+let win = false;
+win = true;
+
+
+let nombreUn = 8, nombreDeux = 4, result; 
+result = (nombreUn + nombreDeux) * 9;
+result += 2;
+result++;
+result--;
+console.log(result); 
+
+
+
+
+
+if(confirm("souhaitez- vous confirmer ?")) {
+    alert("ok");
+
+}
+else { 
+
+    alert("annulation");
+}
+ 
+
+
+const age = prompt("entrez votre age :");
+
+alert("Bonjour, vous avez " + age + "ans");
+
+ 
+
+function direBonjour() {
+    alert("Bonjour Lucie");
+
+}
+direBonjour()
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
